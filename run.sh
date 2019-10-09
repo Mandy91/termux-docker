52b181831933b19d6a110c23aa4a35a7c2e70d8f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-10-10 00:13:59 +0300

    initial commit (x86_64)

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME="termux-x64"
+DOCKER_IMAGE_NAME="xeffyr/termux:x86_64"
+
+docker start "$CONTAINER_NAME" > /dev/null 2> /dev/null || {
+	echo "Creating new container..."
+	docker run \
+		--detach \
+		--name "$CONTAINER_NAME" \
+		--tty \
+		"$DOCKER_IMAGE_NAME"
+}
+
+docker exec --interactive --tty "$CONTAINER_NAME" \
+	/data/data/com.termux/files/usr/bin/login
