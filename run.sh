fd08384329c0f1fd5b23a168cb77a399c284ac7e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-10-10 17:26:32 +0300

    run.sh: fix container name

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONTAINER_NAME="termux"
+CONTAINER_NAME="termux-i686"
 DOCKER_IMAGE_NAME="xeffyr/termux:latest"
 
 docker start "$CONTAINER_NAME" > /dev/null 2> /dev/null || {
